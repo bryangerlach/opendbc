17c2e7425fdeaa154caa86ca96224d328b744c5d
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-05-13 18:52:40 -0500

    .

--- opendbc/safety/safety/safety_hyundai.h
+++ opendbc/safety/safety/safety_hyundai.h
@@ -26,9 +26,9 @@ const LongitudinalLimits HYUNDAI_LONG_LIMITS = {
 };
 
 #define HYUNDAI_COMMON_TX_MSGS(scc_bus, can_canfd_blended) \
-  {0x340, 0,                           8},  /* LKAS11 Bus 0                              */ \
-  {0x4F1, scc_bus,                     4},  /* CLU11 Bus 0 (radar-SCC) or 2 (camera-SCC) */ \
-  {0x485, 0, (can_canfd_blended) ? 8 : 4},  /* LFAHDA_MFC Bus 0, dynamic message size    */ \
+  {0x340, 0,                           8, .check_relay = true},  /* LKAS11 Bus 0                              */ \
+  {0x4F1, scc_bus,                     4, .check_relay = false},  /* CLU11 Bus 0 (radar-SCC) or 2 (camera-SCC) */ \
+  {0x485, 0, (can_canfd_blended) ? 8 : 4, .check_relay = true},  /* LFAHDA_MFC Bus 0, dynamic message size    */ \
 
 #define HYUNDAI_LONG_COMMON_TX_MSGS(scc_bus, can_canfd_blended) \
   HYUNDAI_COMMON_TX_MSGS(scc_bus, can_canfd_blended)                    \
