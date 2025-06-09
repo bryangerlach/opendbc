7c78b1954e1932fc644997472eea43890ef65328
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-06-09 01:08:51 -0500

    .

--- opendbc/car/hyundai/carcontroller.py
+++ opendbc/car/hyundai/carcontroller.py
@@ -141,7 +141,7 @@ class CarController(CarControllerBase, EsccCarController, LongitudinalController
                                                                   torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled,
                                                                   hud_control.leftLaneVisible, hud_control.rightLaneVisible,
                                                                   left_lane_warning, right_lane_warning,
-                                                                  self.lkas_icon, CS.msg_364))
+                                                                  self.lkas_icon))
       can_sends.append(hyundaican.create_msg_364(self.packer, self.frame, self.CP, CS.msg_364))
     else:
       can_sends.append(hyundaican.create_lkas11(self.packer, self.frame, self.CP, apply_torque, apply_steer_req,
