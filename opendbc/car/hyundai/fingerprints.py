ad1d2691d05549a7b06bcd3831710eb5ede6e692
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-06-10 13:30:48 -0500

    add fw

--- opendbc/car/hyundai/fingerprints.py
+++ opendbc/car/hyundai/fingerprints.py
@@ -1163,6 +1163,7 @@ FW_VERSIONS = {
       b'\xf1\x00ON  MFC  AT USA LHD 1.00 1.00 99211-S9160 230303',
       b'\xf1\x00LX2 MFC  AT USA LHD 1.00 1.00 99211-S8600 230317',
       b'\xf1\x00LX2 MFC  AT USA LHD 1.00 1.01 99211-S8600 230817',
+      b'\xf1\x00ON  MFC  AT USA LHD 1.00 1.00 99211-S9170 240531',
     ],
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00LX2_ SCC -----      1.00 1.01 99110-S8150         ',
@@ -1170,6 +1171,7 @@ FW_VERSIONS = {
       b'\xf1\x00LX2_ SCC -----      1.00 1.00 99110-S8150         ',
       b'\xf1\x00ON__ SCC -----      1.00 1.00 99110-S9160         ',
       b'\xf1\x00LX2_ SCC -----      1.00 1.00 99110-S8600         ',
+      b'\xf1\x00ON__ SCC -----      1.00 1.00 99110-S9170         ',
     ],
   },
 }
