9add4024444a594ebb65a71e05d29975529c999c
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-06-27 22:43:26 -0500

    .

--- opendbc/safety/modes/hyundai.h
+++ opendbc/safety/modes/hyundai.h
@@ -382,7 +382,7 @@ static safety_config hyundai_init(uint16_t param) {
   } else if (hyundai_can_canfd_blended) {
     static RxCheck hyundai_can_canfd_blended_rx_checks[] = {
       HYUNDAI_COMMON_RX_CHECKS(false)
-      HYUNDAI_SCC12_ADDR_CHECK(0, true)
+      HYUNDAI_SCC12_ADDR_CHECK(2, true)
     };
 
     ret = BUILD_SAFETY_CFG(hyundai_can_canfd_blended_rx_checks, HYUNDAI_CAN_CANFD_BLENDED_TX_MSGS);
