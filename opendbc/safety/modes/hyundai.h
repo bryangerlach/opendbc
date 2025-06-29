c05f7ec7e3f55dfafd6be89aaee1176ba84aaf36
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-06-29 15:13:28 -0500

    .

--- opendbc/safety/modes/hyundai.h
+++ opendbc/safety/modes/hyundai.h
@@ -316,7 +316,7 @@ static safety_config hyundai_init(uint16_t param) {
 
   static const CanMsg HYUNDAI_LONG_ESCC_TX_MSGS[] = {
     HYUNDAI_LONG_COMMON_TX_MSGS(0, true)
-  }
+  };
 
   hyundai_common_init(param);
   hyundai_legacy = false;
