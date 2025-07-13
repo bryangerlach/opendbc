5b45cebb9e790e7a0d94b24ba121372425e9f87f
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-07-13 10:10:47 -0500

    .

--- opendbc/safety/modes/hyundai.h
+++ opendbc/safety/modes/hyundai.h
@@ -36,6 +36,7 @@ const LongitudinalLimits HYUNDAI_LONG_LIMITS = {
   {0x420, scc_bus,       8, .check_relay = true},   /* SCC11 Bus 0       */ \
   {0x421, scc_bus,       8, .check_relay = true},   /* SCC12 Bus 0       */ \
   {0x389, scc_bus,       8, .check_relay = true},   /* SCC14 Bus 0       */ \
+  {0x4A2, scc_bus,       2, .check_relay = false},  /* FRT_RADAR11 Bus 0 */ \
 
 #define HYUNDAI_COMMON_RX_CHECKS(legacy)                                                                                                                                               \
   {.msg = {{0x260, 0, 8, .max_counter = 3U, .ignore_quality_flag = true, .frequency = 100U},                                                                                           \
@@ -306,7 +307,7 @@ static safety_config hyundai_init(uint16_t param) {
 
   static const CanMsg HYUNDAI_CAN_CANFD_BLENDED_LONG_TX_MSGS[] = {
     HYUNDAI_LONG_COMMON_TX_MSGS(0, true)
-    {0x38D, 0, 8, .check_relay = true}, // FCA11 Bus 0
+    {0x38D, 0, 8, .check_relay = false}, // FCA11 Bus 0
   };
 
   static const CanMsg HYUNDAI_LONG_ESCC_TX_MSGS[] = {
