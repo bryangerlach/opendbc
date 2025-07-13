7caa5b8f134353c57fabe53da9d3b332500f8c90
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-07-12 23:07:25 -0500

    .

--- opendbc/safety/modes/hyundai.h
+++ opendbc/safety/modes/hyundai.h
@@ -305,9 +305,6 @@ static safety_config hyundai_init(uint16_t param) {
     {0x420, 0,       8, .check_relay = false},   /* SCC11 Bus 0       */ \
     {0x421, 0,       8, .check_relay = false},   /* SCC12 Bus 0       */ \
     {0x389, 0,       8, .check_relay = false},   /* SCC14 Bus 0       */ \
-    {0x420, 2,       8, .check_relay = false},   /* SCC11 Bus 0       */ \
-    {0x421, 2,       8, .check_relay = false},   /* SCC12 Bus 0       */ \
-    {0x389, 2,       8, .check_relay = false},   /* SCC14 Bus 0       */ \
   };
 
   static const CanMsg HYUNDAI_CAN_CANFD_BLENDED_LONG_TX_MSGS[] = {
