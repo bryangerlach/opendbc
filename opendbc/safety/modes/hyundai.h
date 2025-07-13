d54fe20c2c417fef4389fd5478b761d24e6338c9
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-07-13 10:31:12 -0500

    .

--- opendbc/safety/modes/hyundai.h
+++ opendbc/safety/modes/hyundai.h
@@ -44,9 +44,9 @@ static const CanMsg HYUNDAI_CAN_CANFD_BLENDED_HDA2_LONG_TX_MSGS[] = {
   {0x730, 1, 8, .check_relay = false},
   {0x340, 1, 8, .check_relay = false},
   {0x485, 1, 8, .check_relay = false},
-  {0x420, 1, 8, .check_relay = true},
-  {0x421, 1, 8, .check_relay = true},
-  {0x389, 1, 8, .check_relay = true},
+  {0x420, 1, 8, .check_relay = false},
+  {0x421, 1, 8, .check_relay = false},
+  {0x389, 1, 8, .check_relay = false},
   {0x38D, 1, 8, .check_relay = false},
   {0x363, 1, 8, .check_relay = false},
   {0x398, 1, 8, .check_relay = false},
@@ -71,6 +71,7 @@ static const CanMsg HYUNDAI_CAN_CANFD_BLENDED_HDA2_LONG_TX_MSGS[] = {
            {0x371, 0, 8, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true, .frequency = 100U}, { 0 }}},                                                    \
   {.msg = {{0x386, (pt_bus), 8, .ignore_checksum = (legacy), .ignore_counter = (legacy), .max_counter = (legacy) ? 0U : 15U, .ignore_quality_flag = true, .frequency = (can_canfd_blended) ? 50U :100U}, { 0 }, { 0 }}}, \
   {.msg = {{0x394, (pt_bus), 8, .ignore_checksum = (legacy), .ignore_counter = (legacy), .max_counter = (legacy) ? 0U : 7U, .ignore_quality_flag = true, .frequency = (can_canfd_blended) ? 50U : 100U}, { 0 }, { 0 }}}, \
+  {.msg = {{0x4F1, (pt_bus), 4, .ignore_checksum = true, .max_counter = 15U, .ignore_quality_flag = true, .frequency = 50U}, { 0 }, { 0 }}},                                                  \
 
 #define HYUNDAI_SCC11_ADDR_CHECK(scc_bus)                                                                                                         \
   {.msg = {{0x420, (scc_bus), 8, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true, .frequency = 50U}, { 0 }, { 0 }}}, \
@@ -100,7 +101,6 @@ RxCheck hyundai_can_canfd_blended_hda2_rx_checks[] = {
 
 RxCheck hyundai_can_canfd_blended_hda2_long_rx_checks[] = {
   HYUNDAI_COMMON_RX_CHECKS(false, true, 1)
-  {.msg = {{0x4F1, 1, 4, .ignore_checksum = true, .max_counter = 15U, .ignore_quality_flag = true, .frequency = 50U}, { 0 }, { 0 }}},
 };
 
 static bool hyundai_legacy = false;
