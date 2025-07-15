2f141bd1cbebd4ff8b449867f7f9deac2c89cbb7
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-07-15 16:09:17 -0500

    .

--- opendbc/car/hyundai/carcontroller.py
+++ opendbc/car/hyundai/carcontroller.py
@@ -108,7 +108,7 @@ class CarController(CarControllerBase, EsccCarController, LongitudinalController
       if self.CP.flags & HyundaiFlags.CANFD_LKA_STEERING.value:
         addr, bus = 0x730, self.CAN.ECAN
       can_sends.append(make_tester_present_msg(addr, bus, suppress_response=True))
-      bus = 0x730
+      addr = 0x730
       can_sends.append(make_tester_present_msg(addr, bus, suppress_response=True))
 
       # for blinkers
