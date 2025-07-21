35f2e583ce4d033de3778a973859c1ab9a9d6269
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-07-20 19:29:38 -0500

    testing radar disable

--- opendbc/safety/modes/hyundai.h
+++ opendbc/safety/modes/hyundai.h
@@ -329,6 +329,9 @@ static safety_config hyundai_init(uint16_t param) {
     {0x420, 0, 8, .check_relay = true, .disable_static_blocking = true},   /* SCC11 Bus 0       */
     {0x421, 0, 8, .check_relay = true, .disable_static_blocking = true},   /* SCC12 Bus 0       */
     {0x389, 0, 8, .check_relay = true, .disable_static_blocking = true},   /* SCC14 Bus 0       */
+    {0x420, 2, 8, .check_relay = true, .disable_static_blocking = true},   /* SCC11 Bus 0       */
+    {0x421, 2, 8, .check_relay = true, .disable_static_blocking = true},   /* SCC12 Bus 0       */
+    {0x389, 2, 8, .check_relay = true, .disable_static_blocking = true},   /* SCC14 Bus 0       */
     {0x38D, 0, 8, .check_relay = false}, // FCA11 Bus 0
     {0x7D0, 0, 8, .check_relay = false}, // radar UDS TX addr Bus 0 (for radar disable)
     {0x7D0, 2, 8, .check_relay = false}, // radar UDS TX addr Bus 0 (for radar disable)
