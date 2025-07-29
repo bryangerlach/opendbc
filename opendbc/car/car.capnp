69888062884fc5928040a71c2bae4e411bece9bf
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-07-29 15:14:00 -0500

    daw testing

--- opendbc/car/car.capnp
+++ opendbc/car/car.capnp
@@ -174,6 +174,8 @@ struct CarState {
   standstill @18 :Bool;
   wheelSpeeds @2 :WheelSpeeds;
 
+  dawStatus @67 :UInt8;
+
   # gas pedal, 0.0-1.0
   gas @3 :Float32;        # this is user pedal only
   gasPressed @4 :Bool;    # this is user pedal only
--- opendbc/car/hyundai/carstate.py
+++ opendbc/car/hyundai/carstate.py
@@ -65,6 +65,7 @@ class CarState(CarStateBase, EsccCarStateBase, MadsCarState, CarStateExt):
 
     self.cruise_info = {}
     self.msg_364 = {}
+    self.dawStatus = 5
 
     # On some cars, CLU15->CF_Clu_VehicleSpeed can oscillate faster than the dash updates. Sample at 5 Hz
     self.cluster_speed = 0
@@ -147,6 +148,7 @@ class CarState(CarStateBase, EsccCarStateBase, MadsCarState, CarStateExt):
     # Consider Taking a Break message
     if self.CP.flags & HyundaiFlags.CAN_CANFD_BLENDED:
       self.msg_364 = copy.copy(cp_cam.vl["ALERTS_364"])
+      self.dawStatus = cp.vl["ALERTS_364"]["DAW_Status"]
 
     # TODO: Find brake pressure
     ret.brake = 0
