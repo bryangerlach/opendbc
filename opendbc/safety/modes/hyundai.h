d34ea87440efb3e86c1dff622002c9b9eb9d890d
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-08-02 13:34:55 -0500

    fix

--- opendbc/safety/modes/hyundai.h
+++ opendbc/safety/modes/hyundai.h
@@ -235,7 +235,7 @@ static bool hyundai_tx_hook(const CANPacket_t *msg) {
                                                             ((msg->data[5] << 3) | (msg->data[4] >> 5)) - 1023U;
 
     int aeb_decel_cmd = hyundai_can_canfd_blended ? 0 : msg->data[2];
-    bool aeb_req = hyundai_can_canfd_blended ? 0 : msg->data[54U];
+    bool aeb_req = hyundai_can_canfd_blended ? 0 : msg->data[54];
 
     acc_main_on_tx = hyundai_can_canfd_blended ? msg->data[27] : msg->data[0];
     hyundai_common_acc_main_on_sync();
