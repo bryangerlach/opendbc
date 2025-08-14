a0e654b6c4fce31225819b660f483189068f0591
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-08-13 19:54:11 -0500

    test mando radar?

--- opendbc/sunnypilot/car/hyundai/enable_radar_tracks.py
+++ opendbc/sunnypilot/car/hyundai/enable_radar_tracks.py
@@ -24,9 +24,20 @@ DEFAULT_CONFIG = bytes([0x00, 0x00, 0x00, 0x01, 0x00, 0x00])
 TRACKS_ENABLED_CONFIG = bytes([0x00, 0x00, 0x00, 0x01, 0x00, 0x01])
 TRACKS_ENABLED_CONFIG_BYTES = b"\x00\x00\x01\x00\x01"
 
+RESET_REQUEST = b'\x11\x01'
+RESET_RESPONSE = b''
+
 
 def enable_radar_tracks(logcan, sendcan, bus=0, addr=0x7d0, timeout=0.1, retry=2):
   carlog.error("radar_tracks: enabling ...")
+
+  try:
+    # Send reset first because the disable request below is not getting to the radar soon enough
+    carlog.error("sending reset (0x11 0x01)")
+    reset_query = IsoTpParallelQuery(sendcan, logcan, bus, [addr], [RESET_REQUEST], [RESET_RESPONSE])
+    reset_query.get_data(timeout=0.1)
+  except Exception:
+    carlog.error("reset failed or unsupported")
 
   for i in range(retry):
     try:
