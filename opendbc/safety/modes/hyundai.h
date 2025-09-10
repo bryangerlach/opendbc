d86c63d668cf7ab3e4b55fe408119c9f00129ebf
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-09-09 22:31:07 -0500

    lda safety on stock long

--- opendbc/safety/modes/hyundai.h
+++ opendbc/safety/modes/hyundai.h
@@ -76,10 +76,12 @@ RxCheck hyundai_rx_checks[] = {
 RxCheck hyundai_can_canfd_blended_hda2_rx_checks[] = {
   HYUNDAI_COMMON_RX_CHECKS(false, true, 1)
   HYUNDAI_SCC12_ADDR_CHECK(true, 1)
+  HYUNDAI_LDA_BUTTON_ADDR_CHECK
 };
 
 RxCheck hyundai_can_canfd_blended_hda2_long_rx_checks[] = {
   HYUNDAI_COMMON_RX_CHECKS(false, true, 1)
+  HYUNDAI_LDA_BUTTON_ADDR_CHECK
 };
 
 static bool hyundai_legacy = false;
