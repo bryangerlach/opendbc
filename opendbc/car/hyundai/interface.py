bb08eb18c1062f4e19dca37127eaf8a9dedd70d5
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-09-15 16:57:06 -0500

    radarunabailable = false

--- opendbc/car/hyundai/interface.py
+++ opendbc/car/hyundai/interface.py
@@ -175,6 +175,7 @@ class CarInterface(CarInterfaceBase):
 
     if ret.flags & HyundaiFlagsSP.ENHANCED_SCC:
       ret.safetyParam |= HyundaiSafetyFlagsSP.ESCC
+      stock_cp.radarUnavailable = False
 
     if stock_cp.flags & HyundaiFlags.HAS_LDA_BUTTON:
       ret.safetyParam |= HyundaiSafetyFlagsSP.HAS_LDA_BUTTON
