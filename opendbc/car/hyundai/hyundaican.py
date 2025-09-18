876097eb09915e770e5c77419c87cab08d3713a3
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-09-18 15:38:04 -0500

    can_canfd acc command

--- opendbc/car/hyundai/hyundaican.py
+++ opendbc/car/hyundai/hyundaican.py
@@ -231,7 +231,7 @@ def create_acc_commands_can_canfd_blended(packer, enabled, accel, upper_jerk, id
       "VSetDis": set_speed if enabled else 0,
       "ACC_ObjDist": int(lead_data.lead_distance),
       "ACCMode": 2 if enabled and long_override else 1 if enabled else 0,
-      "StopReq": 1 if stopping else 0,
+      "StopReq": 1 if tuning.stopping else 0,
     }
 
   def get_scc14_values():
