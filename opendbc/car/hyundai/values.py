224a0f1932718c827ead0cfbd52a270696825352
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-10-24 15:05:56 -0500

    values - steering threshhold/allowance

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -54,10 +54,10 @@ class CarControllerParams:
 
     elif CP.flags & HyundaiFlags.CAN_CANFD_BLENDED:
       self.STEER_MAX = 384
-      self.STEER_DRIVER_ALLOWANCE = 250
+      self.STEER_DRIVER_ALLOWANCE = 50
       self.STEER_THRESHOLD = 250
-      self.STEER_DELTA_UP = 2
-      self.STEER_DELTA_DOWN = 3
+      self.STEER_DELTA_UP = 3
+      self.STEER_DELTA_DOWN = 7
 
     # Default for most HKG
     else:
