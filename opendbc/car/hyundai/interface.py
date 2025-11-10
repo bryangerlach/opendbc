1aa9ebe5be1a98301211dccf52e52dbd813474f8
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-11-10 14:01:15 -0600

    test radar disable

--- opendbc/car/hyundai/interface.py
+++ opendbc/car/hyundai/interface.py
@@ -183,8 +183,7 @@ class CarInterface(CarInterfaceBase):
     if not stock_cp.flags & HyundaiFlags.CANFD:
       # TODO-SP: add route with ESCC message for process replay
       if ESCC_MSG in fingerprint[0]:
-        if not stock_cp.flags & HyundaiFlags.CAN_CANFD_BLENDED:
-          ret.flags |= HyundaiFlagsSP.ENHANCED_SCC.value
+        ret.flags |= HyundaiFlagsSP.ENHANCED_SCC.value
 
     if ret.flags & HyundaiFlagsSP.ENHANCED_SCC:
       ret.safetyParam |= HyundaiSafetyFlagsSP.ESCC
