3a8903b00e1d7984052f2131d227b5930fe000c7
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2026-02-24 12:38:24 -0600

    stopping

--- opendbc/car/hyundai/interface.py
+++ opendbc/car/hyundai/interface.py
@@ -146,7 +146,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelay = 0.5
 
     if ret.flags & HyundaiFlags.CAN_CANFD_BLENDED:
-      ret.stoppingDecelRate = 0.2
+      ret.stoppingDecelRate = 0.1
 
     if ret.openpilotLongitudinalControl:
       ret.safetyConfigs[-1].safetyParam |= HyundaiSafetyFlags.LONG.value
