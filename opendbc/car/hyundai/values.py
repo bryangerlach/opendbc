9ab87b6a2e8ab83320dddb3bcb45fb528f452867
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2026-04-11 21:16:06 -0500

    torque settings

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -53,7 +53,7 @@ class CarControllerParams:
       self.STEER_DELTA_DOWN = 3
 
     elif CP.flags & HyundaiFlags.CAN_CANFD_BLENDED:
-      self.STEER_MAX = 384
+      self.STEER_MAX = 270
       self.STEER_DRIVER_ALLOWANCE = 50
       self.STEER_THRESHOLD = 150
       self.STEER_DELTA_UP = 2
