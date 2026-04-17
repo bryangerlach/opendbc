78d03978dbf675ee5b68439617eeb352e1613061
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2026-04-17 14:16:29 -0500

    damp factor

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -48,7 +48,7 @@ def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_torque,
     "LKA_ASSIST": 0,
     "STEER_REQ": 1 if lat_active else 0,
     "HAS_LANE_SAFETY": 0,  # hide LKAS settings
-    "DAMP_FACTOR": 50 if vEgo < 17.8816 else 100,  # can potentially tuned for better perf [3, 200]
+    "DAMP_FACTOR": 50 if vEgo < 17.8816 else 80,  # can potentially tuned for better perf [3, 200]
   }
 
   lkas_values = copy.copy(common_values)
